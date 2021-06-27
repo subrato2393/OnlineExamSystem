cb3cfc0a4b2b211d03f3fc6763ca117951613d87
Subrato Ghosh <subrato2393@gmail.com>
2021-06-28 00:41:55 +0600

    Redirect correction organization registration page

--- source/OnlineExam/Applications/OnlineExam.Web/Controllers/AccountController.cs
+++ source/OnlineExam/Applications/OnlineExam.Web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.WebUtilities;
@@ -45,41 +46,91 @@ namespace OnlineExam.Web.Controllers
             return View();
         }
 
-        public IActionResult OrganizationLogin()
+        public async Task<IActionResult> OrganizationLogin(string returnUrl = null)
         {
-            return View();
+            var model = new LoginModel();
+            if (!string.IsNullOrEmpty(model.ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, model.ErrorMessage);
+            }
+
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            model.ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
+            model.ReturnUrl = returnUrl;
+            return View(model);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> OrganizationLogin(LoginModel model, string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    _logger.LogInformation("User logged in.");
+                    return RedirectToAction("Index", "Home", new { Area = "member" });
+                    //return LocalRedirect(returnUrl);
+                }
+                if (result.RequiresTwoFactor)
+                {
+                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                }
+                if (result.IsLockedOut)
+                {
+                    _logger.LogWarning("User account locked out.");
+                    return RedirectToPage("./Lockout");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View();
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
 
         public async Task<IActionResult> OrganizationSignUp(string returnUrl = null)
         {
-            var model =new RegisterModel();
+            var model = new RegisterModel();
             model.ReturnUrl = returnUrl;
             model.ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             return View(model);
         }
 
         [HttpPost]
-        public async Task<IActionResult> OrganizationSignUp(RegisterModel model,string returnUrl = null)
+        public async Task<IActionResult> OrganizationSignUp(RegisterModel model, string returnUrl = null)
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             model.ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser
-                { 
-                    UserName = model.Email, 
+                {
+                    UserName = model.Email,
                     Email = model.Email,
-                    About=model.About,
-                    Code=model.Code,
+                    About = model.About,
+                    Code = model.Code,
                     Address = model.Address,
-                    Name=model.Name,
-                    PostalCode=model.PostalCode,
+                    Name = model.Name,
+                    PostalCode = model.PostalCode,
                 };
                 var result = await _userManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+                    return RedirectToAction("Index", "Home", new { Area = "member" });
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
@@ -94,7 +145,7 @@ namespace OnlineExam.Web.Controllers
 
                     if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
-                        return RedirectToPage("RegisterConfirmation", new { email = model.Email, returnUrl = returnUrl });
+                        return RedirectToAction("Index","Home", new { Area= "member"});
                     }
                     else
                     {
