6c70901c96987330b3b7341007889f17ba4e859e
Subrato Ghosh <subrato2393@gmail.com>
2021-07-05 15:28:45 +0600

    adding Data Access Layar library code

---
+++ source/OnlineExam/OnlineExam.DataAccessLayar/IEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineExam.DataAccessLayar
+{
+    public interface IEntity<T>
+    {
+        T Id { get; set; }
+    }
+}
---
+++ source/OnlineExam/OnlineExam.DataAccessLayar/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace OnlineExam.DataAccessLayar
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Save();
+    }
+}
